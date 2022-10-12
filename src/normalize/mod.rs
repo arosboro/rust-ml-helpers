e363e71c613ed75c12ec09c3c793d1f817b793e9
Andrew Rosborough <arosborough@oculat.us>
2022-10-11 20:23:54 -0400

    Update normalize.

--- src/normalize/mod.rs
+++ src/normalize/mod.rs
@@ -2,8 +2,8 @@ use std::collections::HashMap;
 
 type Dictionary = HashMap<String, f64>;
 
-pub fn build_dictionary(mut dictionary: Dictionary, text: Vec<String>) -> Dictionary {
-    for term in text {
+pub fn build_dictionary(mut dictionary: Dictionary, document: Vec<String>) -> Dictionary {
+    for term in document {
         if !dictionary.contains_key(&term.to_string()) {
             dictionary.insert(term.to_string(), dictionary.len() as f64);
         }
@@ -13,13 +13,13 @@ pub fn build_dictionary(mut dictionary: Dictionary, text: Vec<String>) -> Dictio
 
 pub fn build_document_frequency_overall_frequency(
     mut overall_frequency: Dictionary,
-    text: &Vec<String>,
+    document: &Vec<String>,
 ) -> (Dictionary, Dictionary) {
-    let document_frequency = text
+    let document_frequency = document
         .iter()
-        .fold(HashMap::new(), |mut acc: Dictionary, token| {
-            *acc.entry(token.to_owned()).or_insert(0.0) += 1.0;
-            *overall_frequency.entry(token.to_owned()).or_insert(0.0) += 1.0;
+        .fold(HashMap::new(), |mut acc: Dictionary, term| {
+            *acc.entry(term.to_owned()).or_insert(0.0) += 1.0;
+            *overall_frequency.entry(term.to_owned()).or_insert(0.0) += 1.0;
             acc
         });
     (document_frequency, overall_frequency)
