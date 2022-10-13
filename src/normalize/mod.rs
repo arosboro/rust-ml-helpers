b854453aab99ff52da5b98260f39cae140d5f830
Andrew Rosborough <arosborough@oculat.us>
2022-10-13 06:44:01 -0400

    Classifier frequency.

--- src/normalize/mod.rs
+++ src/normalize/mod.rs
@@ -27,6 +27,24 @@ pub fn build_document_frequency_overall_frequency(
     document_frequency
 }
 
+pub fn build_classifier_frequency(
+    classifier_frequency: &mut Vec<HashMap<String, f64>>,
+    classifiers: &Vec<u8>,
+    classifier: &u8,
+    document_frequency: &HashMap<String, f64>,
+) -> () {
+    for indicator in classifiers {
+        if classifier == indicator {
+            for (term, count) in document_frequency.iter() {
+                let index = classifiers.iter().position(|&x| x == *classifier).unwrap();
+                *classifier_frequency[index]
+                    .entry(term.to_owned())
+                    .or_insert(0.0) += count;
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -94,5 +112,75 @@ mod tests {
                 assert_eq!(actual_overall_frequency, expected_overall_frequency);
             }
         }
+    }
+
+    #[test]
+    fn test_build_classifier_frequency() {
+        let mut actual_overall_frequency: HashMap<String, f64> = HashMap::new();
+        let expected_overall_frequency: HashMap<String, f64> = vec![
+            ("a".to_string(), 3.0),
+            ("big".to_string(), 1.0),
+            ("red".to_string(), 1.0),
+            ("dog".to_string(), 1.0),
+            ("cat".to_string(), 1.0),
+            ("small".to_string(), 1.0),
+            ("blue".to_string(), 2.0),
+            ("boobie".to_string(), 1.0),
+        ]
+        .into_iter()
+        .collect();
+        let expected_classifier_frequency: Vec<HashMap<String, f64>> = vec![
+            vec![
+                ("a".to_string(), 1.0),
+                ("blue".to_string(), 1.0),
+                ("boobie".to_string(), 1.0),
+            ]
+            .into_iter()
+            .collect(),
+            vec![
+                ("a".to_string(), 2.0),
+                ("cat".to_string(), 1.0),
+                ("small".to_string(), 1.0),
+                ("blue".to_string(), 1.0),
+                ("big".to_string(), 1.0),
+                ("red".to_string(), 1.0),
+                ("dog".to_string(), 1.0),
+            ]
+            .into_iter()
+            .collect(),
+            vec![].into_iter().collect(),
+        ];
+        let document1: Vec<String> = vec![
+            "a".to_string(),
+            "big".to_string(),
+            "red".to_string(),
+            "dog".to_string(),
+        ];
+        let document2: Vec<String> = vec![
+            "a".to_string(),
+            "small".to_string(),
+            "blue".to_string(),
+            "cat".to_string(),
+        ];
+        let document3: Vec<String> =
+            vec!["a".to_string(), "blue".to_string(), "boobie".to_string()];
+        let documents_x = vec![document1, document2, document3];
+        let classifiers_y: Vec<u8> = vec![2, 2, 1];
+        let classifiers = vec![1, 2, 3];
+        let mut classifier_frequency: Vec<HashMap<String, f64>> = vec![HashMap::new(); 3];
+        for (i, document) in documents_x.iter().enumerate() {
+            let document_frequency = build_document_frequency_overall_frequency(
+                &mut actual_overall_frequency,
+                &document,
+            );
+            build_classifier_frequency(
+                &mut classifier_frequency,
+                &classifiers,
+                &classifiers_y[i],
+                &document_frequency,
+            );
+        }
+        assert_eq!(classifier_frequency, expected_classifier_frequency);
+        assert_eq!(actual_overall_frequency, expected_overall_frequency);
     }
 }
